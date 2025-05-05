8e62df61c4f6eec0b9a23304270767c980c4545d
ReggieMW <reggiewoods@msn.com>
2025-05-05 14:49:13 -0500

    added 4 students

--- Module2Review/Program.cs
+++ Module2Review/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        List<string> students = new List<string>();
+        
+        Student michael = new Student();
+        michael.Name = "Michael Doyle";
+        michael.ID = 205;
+        Student reggie = new Student() { Name = "Reggie Woods", ID = 206 };
+        Student emily = new Student()
+        {
+            Name = "Emily Woods",
+            ID = 207
+        };
+        Student mookie = new Student() { Name = "Mookie Woods", ID = 208 };
+
+        students.Add(michael.Name); 
+        students.Add(reggie.Name);
+        students.Add(emily.Name);
+        students.Add(mookie.Name);
+        
+        
     }
 }
